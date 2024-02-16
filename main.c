fb880b99d7b739543c6dc30397ad5283dfc49bfa
AdolfMkonda <adolfanatory@gmail.com>
2024-02-17 02:45:37 +0300

    read user input

--- main.c
+++ main.c
@@ -8,9 +8,13 @@
 
 int main(void)
 {
+	char *command = NULL;
+	size_t cmdlen = 0;
+
 	while (!0)
 	{
 		write(STDOUT_FILENO, "ado##: ", 8);
+		commands(&command, &cmdlen, stdin);
 	}
 
 	return (0);
--- shell.h
+++ shell.h
@@ -1,2 +1,17 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+
+
+
+void commands(char **command, size_t *cmdlen, FILE *stream);
+
+
+
+
+
+#endif

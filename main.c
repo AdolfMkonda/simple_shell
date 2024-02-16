cfa884439fab8a82da9fef3418c29d516eaff5f3
AdolfMkonda <adolfanatory@gmail.com>
2024-02-16 18:48:42 +0300

    shell prompt

---
+++ main.c
@@ -0,0 +1,17 @@
+#include "shell.h"
+
+/**
+ * main - Is the starting point of the shell
+ * ....
+ * Return: always 0
+ */
+
+int main(void)
+{
+	while (!0)
+	{
+		write(STDOUT_FILENO, "ado##: ", 8);
+	}
+
+	return (0);
+}

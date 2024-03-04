d1317eaedadd3cdb0d7057b36e8a4f4484cdc7de
AdolfMkonda <adolfanatory@gmail.com>
2024-03-04 21:57:28 +0300

    strtok fixx

--- strtok.c
+++ strtok.c
@@ -13,33 +13,23 @@ char *_strtok(char *str, const char *delim)
 	char *token_start = ptr;
 
 	if (str != NULL)
-	{
 		ptr = str;
-	}
 	if (ptr == NULL)
-	{
 		return (NULL);
-	}
-
 	while (*ptr != '\0' && strchr(delim, *ptr) != NULL)
 	{
 		++ptr;
 	}
 
 	if (*ptr == '\0')
-	{
 		return (NULL);
-	}
-
 	while (*ptr != '\0' && strchr(delim, *ptr) == NULL)
 	{
 		++ptr;
 	}
 
 	if (*ptr == '\0')
-	{
 		ptr = NULL;
-	}
 	else
 	{
 		*ptr = '\0';
